c4ff30e2459b9fa3690478aa9496f43c955f0768
Susanne Nilsson <45233211+kiwiguard@users.noreply.github.com>
2021-02-17 14:52:57 +0100

    more responsive design, started on mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,1 +1,13 @@
 // Mobile-menu scripts
+var navList = document.getElementById('mobile-nav-list');
+var button = document.getElementById('mobile-menu-btn')
+
+button.addEventListener('click', function() {
+    if (navList.classList.contains('is-active')) {
+        this.setAttribute('aria-expanded', false);
+        navList.classList.remove('is-active');
+    } else {
+        navList.classList.add('is-active');
+        this.setAttribute('aria-expanded', 'true');
+    }
+});

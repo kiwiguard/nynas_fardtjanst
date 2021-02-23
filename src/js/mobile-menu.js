40cbb5cf835276f013a5bbf6761a3f27a43a55b0
Susanne Nilsson <45233211+kiwiguard@users.noreply.github.com>
2021-02-23 16:01:39 +0100

    fully functional mobile menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,12 +2,24 @@
 var navList = document.getElementById('mobile-nav-list');
 var button = document.getElementById('mobile-menu-btn')
 
-button.addEventListener('click', function() {
-    if (navList.classList.contains('is-active')) {
-        this.setAttribute('aria-expanded', false);
-        navList.classList.remove('is-active');
-    } else {
-        navList.classList.add('is-active');
-        this.setAttribute('aria-expanded', 'true');
-    }
-});
+// function openNav() {
+//     document.getElementById("mobile-nav").style.width = "100%";
+//     document.getElementById("main").style.display = "none";
+// }
+
+// function closeNav() {
+//     document.getElementById("mobile-nav").style.width = "0";
+//     document.getElementById("main").style.display = "block";
+// }
+
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+    document.getElementById("main").style.display = "none";
+    document.getElementById("mobile-nav").setAttribute('aria-expanded', true);
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+    document.getElementById("main").style.display = "block";
+    document.getElementById("mobile-nav").setAttribute('aria-expanded', false);
+}
